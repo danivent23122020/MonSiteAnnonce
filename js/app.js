b59f8150ff05099380844e1205303767c5e76959
danivent23122020 <dani.vent@orange.fr>
2022-09-10 23:02:42 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ btnNav.addEventListener("click", () => {
     listNav.classList.toggle("active-nav");
     //
     if (imgBtn.src.includes("menu")) {
-        imgBtn.src = "./ressources/croix.svg";
+        imgBtn.src = "../ressources/croix.svg";
         imgBtn.style.width = "30px";
         imgBtn.style.height = "30px";
     } else {
-        imgBtn.src = "./ressources/menu.svg";
+        imgBtn.src = "../ressources/menu.svg";
         imgBtn.style.width = "40px";
         imgBtn.style.height = "40px";
     }
