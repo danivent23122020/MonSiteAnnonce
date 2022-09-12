542404a175bfd89e9996834df2a7b3e1a35e555b
danivent23122020 <dani.vent@orange.fr>
2022-09-12 09:35:03 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -6,15 +6,27 @@ let imgBtn = document.querySelector(".responsive-nav-btn img");
 btnNav.addEventListener("click", () => {
     //
     listNav.classList.toggle("active-nav");
-    //
-    if (imgBtn.src.includes("menu")) {
-        imgBtn.src = "../ressources/croix.svg";
-        imgBtn.style.width = "30px";
-        imgBtn.style.height = "30px";
+    // condition display croix/menu.svg index/others
+    if (window.location.href.includes("index")) {
+        if (imgBtn.src.includes("menu")) {
+            imgBtn.src = "./ressources/croix.svg";
+            imgBtn.style.width = "30px";
+            imgBtn.style.height = "30px";
+        } else {
+            imgBtn.src = "./ressources/menu.svg";
+            imgBtn.style.width = "40px";
+            imgBtn.style.height = "40px";
+        }
     } else {
-        imgBtn.src = "../ressources/menu.svg";
-        imgBtn.style.width = "40px";
-        imgBtn.style.height = "40px";
+        if (imgBtn.src.includes("menu")) {
+            imgBtn.src = "../ressources/croix.svg";
+            imgBtn.style.width = "30px";
+            imgBtn.style.height = "30px";
+        } else {
+            imgBtn.src = "../ressources/menu.svg";
+            imgBtn.style.width = "40px";
+            imgBtn.style.height = "40px";
+        }
     }
 });
 // FAQ animation
