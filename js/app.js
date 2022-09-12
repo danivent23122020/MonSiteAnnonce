b65d3835573dfc34af85d3a35e1ad2844ba82936
danivent23122020 <dani.vent@orange.fr>
2022-09-12 11:58:24 +0200

    modif

--- js/app.js
+++ js/app.js
@@ -49,3 +49,22 @@ allCross.forEach((logo) => {
         }
     });
 });
+// Onglets animation
+const choixTarifs = Array.from(document.querySelectorAll(".choix"));
+const contenuTarifs = Array.from(document.querySelectorAll(".panneau"));
+//
+choixTarifs.forEach((choix) => {
+    choix.addEventListener("click", (index) => {
+        let indexClic = choixTarifs.indexOf(index.target);
+        //
+        for (let i = 0; i < choixTarifs.length; i++) {
+            if (i === indexClic) {
+                choixTarifs[i].classList.add("active-choix");
+                contenuTarifs[i].classList.add("panneau-active");
+            } else {
+                choixTarifs[i].classList.remove("active-choix");
+                contenuTarifs[i].classList.remove("panneau-active");
+            }
+        }
+    });
+});
