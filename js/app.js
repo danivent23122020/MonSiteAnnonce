55684ae3097ddd6878d5ed05036a2008960f7858
danivent23122020 <dani.vent@orange.fr>
2022-10-06 11:24:11 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -68,3 +68,19 @@ choixTarifs.forEach((choix) => {
         }
     });
 });
+
+// *** scroll to top ***
+//display btn
+window.addEventListener("scroll", function () {
+    let scroll = this.document.querySelector(".btn-scrollup");
+    scroll.classList.toggle("active", window.scrollY > 200);
+});
+// scroll to top
+const btn = document.querySelector(".btn-scrollup");
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
