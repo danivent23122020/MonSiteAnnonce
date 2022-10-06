752e2bfcf6af71320c602b938cd6af8aa81ed3d8
danivent23122020 <dani.vent@orange.fr>
2022-10-06 20:08:02 +0200

    commit

--- js/app.js
+++ js/app.js
@@ -84,3 +84,35 @@ btn.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+// *** cookie modal ***
+let cookieModal = document.querySelector("#cookieModal");
+let acceptCookie = document.querySelector("#acceptCookie");
+let cancelCookie = document.querySelector("#refusedCookie");
+//
+cancelCookie.addEventListener("click", function () {
+    cookieModal.classList.remove("active");
+});
+//
+acceptCookie.addEventListener("click", function () {
+    let d = new Date();
+    d.setMinutes(2 + d.getMinutes());
+    document.cookie = "HebergeurActos=true; expires = ' + d + ';";
+    cookieModal.classList.remove("active");
+});
+
+// check cookie exist
+const checkCookie = () => {
+    let input = document.cookie.split("=");
+    if (input[0] == "HebergeurActos") {
+        cookieModal.classList.remove("active");
+    } else {
+        cookieModal.classList.add("active");
+    }
+};
+// check cookie exist page already load
+window.onload = () => {
+    setTimeout(() => {
+        checkCookie();
+    }, 1000);
+};
